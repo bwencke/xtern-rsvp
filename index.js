e9e71227a7b6d1657b6e30e3293965c9ddafdb59
Ben Wencke <benwencke@gmail.com>
2015-06-02 23:02:41 -0400

    read JSON request

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json()
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -8,9 +12,9 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-app.post('/rsvp', function(request, response) {
-  console.log("rsvp received");
-  response.send('Hello World!');
+app.post('/rsvp', jsonParser, function(req, res) {
+  console.log(req);
+  res.send(req.body.user_name)
 });
 
 app.listen(app.get('port'), function() {
