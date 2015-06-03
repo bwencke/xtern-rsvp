a1c1da242cf35c61331b55e22d7bb213089cedf4
Ben Wencke <benwencke@gmail.com>
2015-06-02 23:54:38 -0400

    Party Time Event

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+//var mongoose = require('mongoose');
+//var Schema = mongoose.Schema;
+//mongoose.connect('mongodb://bwencke:Sally69@ds043002.mongolab.com:43002/heroku_app37457141');
+//var conn = mongoose.connection;
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,19 +17,37 @@ app.get('/', function(request, response) {
 
 app.post('/rsvp', function(req, res) {
   var userName = req.body.user_name;
-  var eventName = "pizza";
+  var eventId = req.body.text;
+  var eventName = getEventName(eventId);
+  var text;
+  if(eventName == null) {
+  	text = "Sorry, we couldn't find an event with the id \"" + eventId + "\".";
+  } else {
+  	//var user = { "user_name": userName };
+  	//conn.collection(eventId).insert(user);
+  	text = 'Successfully RSVPed for ' + eventName + '! ';
+  }
   var botPayload = {
-    text : 'Successfully RSVPed for ' + eventName + '! ' + req.body.command
+    text : text
   };
 
   // avoid infinite loop
-  if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
-  }
+  // if (userName !== 'slackbot') {
+  //   return res.status(200).json(botPayload);
+  // } else {
+  //   return res.status(200).end();
+  // }
+  res.send(text);
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function getEventName(eventId) {
+	if(eventId == "party") {
+		return "Party Time";
+	} else {
+		return null;
+	}
+}
