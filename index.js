b80c2d34a4b30117dfd3e9dad9644c285d657cf4
Ben Wencke <benwencke@gmail.com>
2015-06-09 22:56:04 -0400

    added sass

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post('/event', function(req, res) {
       var text = "No one has RSVPed for \"" + event.name + "\". Better luck next time.";
       if(event.people && event.people.length > 0) {
         if(event.people.length == 1) {
-          text = "Only 1 super awesome peep has RSVPed for " + event.name + ":\n";
+          text = "Only 1 super awesome peep has RSVPed for \"" + event.name + "\":\n";
         } else {
           text = event.people.length + " awesome peeps have RSVPed for \"" + event.name + "\":\n";
         }
